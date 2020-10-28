8b1242346e36b375cb25ed48441c93415e9ae6c3
Lizzy <lizzy@theender.net>
2020-10-28 15:35:27 +0000

    Added "allow notify" checkbox to submission page and code cleanup
    The submission page now has a checkbox on it to toggle notification permission, which is opt-in.
    
    Also cleaned up the code that runs the POST portion of the submission page, using the .get() function of the MultiKeyDict things with defaults to fall back to if the entries are not there (like when submitting the form with disabled entries).
    Simplified the code a bit as well, rather than adding random bits of logic to define what gets passed to the template engine's context, it's now just individual lines per section.
    
    Resolves #3

--- FloofGang/views.py
+++ FloofGang/views.py
@@ -49,43 +49,47 @@ class Submissions(View):
                       )
 
     def post(self, request):
-        uname = request.POST['uname']
-        day = int(request.POST['bday'])
-        month = int(request.POST['bmon'])
-        error_msg = f"Invalid day ({day}) specified for month {month}. Your response has not been recorded."
+        uname = request.POST.get('uname', "")
+        day = int(request.POST.get('bday', -1))
+        month = int(request.POST.get('bmon', -1))
+        notify = bool(request.POST.get('noti', False))
+
         entry: Birthday = None
+        context = dict(logged_in=request.user.is_authenticated, days=self.days, months=self.months, _STATUS_CODE=None)
         if request.user.is_authenticated:
             try:
-                if month in [4, 6, 9, 11] and day > 30:
-                    raise UserInputError
-                elif month == 2 and day > 29:
-                    raise UserInputError
+                entry = Birthday.objects.get(user=request.user)
+                if f"{day:02}{month:02}" in (entry.birthday, "-1-1"):
+                    entry.notify = notify
+                    entry.save()
+                    messages.add_message(request, messages.SUCCESS, "Successfully updated notification preferences")
+                else:
+                    messages.add_message(request, messages.WARNING, "Day and Month values provided in the form don't match with existing entry. "
+                                                                    "Did you manually edit the HTML?")
+                context = {**context, **{"disabled": True, "class_string": "form-complete", "entry": entry}}
 
-                entry = Birthday.objects.create(user=request.user, display_name=uname, birthday=f"{day:02}{month:02}")
-            except IntegrityError:
-                messages.add_message(request, messages.ERROR, "Duplicate entry exists!?")
-            except UserInputError:
-                messages.error(request, error_msg)
-            except:
-                raise
-            else:
-                messages.add_message(request, messages.SUCCESS, "Entry successfully recorded!")
+            except Birthday.DoesNotExist:
+                # The birthday object didn't already exist so this is likely not an update
+                try:
+                    if month in [4, 6, 9, 11] and day > 30:
+                        raise UserInputError
+                    elif month == 2 and day > 29:
+                        raise UserInputError
+                    entry = Birthday.objects.create(user=request.user, display_name=uname, birthday=f"{day:02}{month:02}", notify=notify)
+                except IntegrityError:  # OPTIMISE This might not be needed anymore?
+                    messages.add_message(request, messages.ERROR, "Duplicate entry exists!?")  # TODO Add error notification stuff here?
+                except UserInputError:
+                    messages.error(request, f"Invalid day ({day}) specified for month {month}. Your response has not been recorded.")
+                    context = {**context, **{"disabled": False, "class_string": None, "entry": None, "_STATUS_CODE": 422}}
+                except:
+                    raise
+                else:
+                    messages.add_message(request, messages.SUCCESS, "Entry successfully recorded!")
+                    context = {**context, **{"disabled": True, "class_string": "form-complete", "entry": entry}}
+        else:
+            context = {**context, **{"disabled": True, "class_string": "form-nologin", "entry": None}}
 
-        class_string = None
-        if request.user.is_authenticated and entry:
-            class_string = 'form-complete'
-        elif not request.user.is_authenticated:
-            class_string = 'form-nologin'
-        return render(request, "birthday/submission.html",
-                      context=dict(logged_in=request.user.is_authenticated,
-                                   disabled=(not request.user.is_authenticated or entry),
-                                   class_string=class_string,
-                                   entry=entry,
-                                   days=self.days,
-                                   months=self.months
-                                   ),
-                      status=403
-                      )
+        return render(request, "birthday/submission.html", context=context, status=context.get("_STATUS_CODE") or 200)
 
 
 def logout_view(request):
